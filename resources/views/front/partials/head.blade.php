5811659fd401dd8478e89bead0aeec68210ec858
alshahriar526 <shahriarnes526@gmail.com>
2026-08-19 02:27:58 +0600

    update slide | topbar | index

--- resources/views/front/partials/head.blade.php
+++ resources/views/front/partials/head.blade.php
@@ -10,7 +10,7 @@
 
 <title>EV4GH</title>
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+{{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
 <!-- Fundraise Up: the new standard for online giving -->
 <script>(function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track,openCheckout'
--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-
-            refresh: [
-                'resources/views/**',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
         }),
     ],
 });
