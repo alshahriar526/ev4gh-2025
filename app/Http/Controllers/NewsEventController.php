2ee579d60eec74ec2236ff1157987acb4bc30488
alshahriar526 <shahriarnes526@gmail.com>
2026-08-28 02:16:24 +0600

    slider issue

--- app/Http/Controllers/NewsEventController.php
+++ app/Http/Controllers/NewsEventController.php
@@ -13,7 +13,7 @@ class NewsEventController extends Controller
         $newsEvents = NewsEvent::orderBy('created_at', 'desc')->get();
 
         return response()
-        ->view('front.index', compact('newsEvents')) // Replace with your actual view name
+        ->view('front.page.index', compact('newsEvents')) // Replace with your actual view name
         ->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
         ->header('Pragma', 'no-cache')
         ->header('Expires', 'Sat, 01 Jan 1990 00:00:00 GMT');
--- resources/views/front/partials/head.blade.php
+++ resources/views/front/partials/head.blade.php
@@ -4,9 +4,10 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- meta -->
-<meta name="description" content="BRAC James P Grant School of Public Health, BRAC University was founded in 2004 in Dhaka, Bangladesh to address the unmet public health challenges particular to Asia, Africa and South America.">
+<meta name="description"
+    content="BRAC James P Grant School of Public Health, BRAC University was founded in 2004 in Dhaka, Bangladesh to address the unmet public health challenges particular to Asia, Africa and South America.">
 
-<link rel="icon" href="{{asset('assets/images/favicon_io/favicon-32x32.png')}}" type="image/x-icon" />
+<link rel="icon" href="{{ asset('assets/images/favicon_io/favicon-32x32.png') }}" type="image/x-icon" />
 
 <title>EV4GH</title>
 
@@ -48,13 +49,31 @@
 <meta http-equiv="Expires" content="0" />
 
 <!-- Fundraise Up Script -->
-<script>(function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track,openCheckout'
-.split(','),i,o=function(n){return'function'==typeof n?o.l.push([arguments])&&o
-:function(){return o.l.push([n,arguments])&&o}},t=d.getElementsByTagName(s)[0],
-j=d.createElement(s);j.async=!0;j.src='https://cdn.fundraiseup.com/widget/'+a+'';
-t.parentNode.insertBefore(j,t);o.s=Date.now();o.v=5;o.h=w.location.href;o.l=[];
-for(i=0;i<8;i++)o[l[i]]=o(l[i]);w[n]=o}
-})(window,document,'script','FundraiseUp','AGXVNSSK');</script>
+<script>
+    (function(w, d, s, n, a) {
+        if (!w[n]) {
+            var l = 'call,catch,on,once,set,then,track,openCheckout'
+                .split(','),
+                i, o = function(n) {
+                    return 'function' == typeof n ? o.l.push([arguments]) && o :
+                        function() {
+                            return o.l.push([n, arguments]) && o
+                        }
+                },
+                t = d.getElementsByTagName(s)[0],
+                j = d.createElement(s);
+            j.async = !0;
+            j.src = 'https://cdn.fundraiseup.com/widget/' + a + '';
+            t.parentNode.insertBefore(j, t);
+            o.s = Date.now();
+            o.v = 5;
+            o.h = w.location.href;
+            o.l = [];
+            for (i = 0; i < 8; i++) o[l[i]] = o(l[i]);
+            w[n] = o
+        }
+    })(window, document, 'script', 'FundraiseUp', 'AGXVNSSK');
+</script>
 
 @include('front.partials.top-inc')
 @yield('styles')
